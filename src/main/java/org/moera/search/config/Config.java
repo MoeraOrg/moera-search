57c3ea59acd98e2d8159c51300f99b814cdb2764
Shmuel Melamud <smelamud@redhat.com>
2025-03-27 02:19:16 +0200

    Implement whoami request.

---
+++ src/main/java/org/moera/search/config/Config.java
@@ -0,0 +1,38 @@
+package org.moera.search.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("search")
+public class Config {
+
+    private String nodeName;
+    private String nodeFullName;
+    private String nodeTitle;
+
+    public String getNodeName() {
+        return nodeName;
+    }
+
+    public void setNodeName(String nodeName) {
+        this.nodeName = nodeName;
+    }
+
+    public String getNodeFullName() {
+        return nodeFullName;
+    }
+
+    public void setNodeFullName(String nodeFullName) {
+        this.nodeFullName = nodeFullName;
+    }
+
+    public String getNodeTitle() {
+        return nodeTitle;
+    }
+
+    public void setNodeTitle(String nodeTitle) {
+        this.nodeTitle = nodeTitle;
+    }
+
+}
---
+++ src/main/java/org/moera/search/rest/WhoAmIiController.java
@@ -0,0 +1,36 @@
+package org.moera.search.rest;
+
+import jakarta.inject.Inject;
+
+import org.moera.lib.node.types.WhoAmI;
+import org.moera.search.config.Config;
+import org.moera.search.global.ApiController;
+import org.moera.search.global.NoCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@ApiController
+@RequestMapping("/moera/api/whoami")
+@NoCache
+public class WhoAmIiController {
+
+    private static final Logger log = LoggerFactory.getLogger(WhoAmIiController.class);
+
+    @Inject
+    private Config config;
+
+    @GetMapping
+    public WhoAmI get() {
+        log.info("GET /whoami");
+
+        WhoAmI whoAmI = new WhoAmI();
+        whoAmI.setNodeName(config.getNodeName());
+        whoAmI.setFullName(config.getNodeFullName());
+        whoAmI.setTitle(config.getNodeTitle());
+
+        return whoAmI;
+    }
+
+}
