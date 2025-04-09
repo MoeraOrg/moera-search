7d56bf4e3bd016cf61f41a95a9387a42133e1f5e
Shmuel Melamud <smelamud@redhat.com>
2025-04-10 00:22:39 +0300

    Home page stub.

---
+++ src/main/java/org/moera/search/ui/IndexUiController.java
@@ -0,0 +1,21 @@
+package org.moera.search.ui;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.moera.search.util.UriUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexUiController {
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request, Model model) {
+        String endpointUri = UriUtil.createBuilderFromRequest(request).path("/moera/").build().toUriString();
+        model.addAttribute("endpointUri", endpointUri);
+
+        return "index";
+    }
+
+}
--- src/main/java/org/moera/search/util/UriUtil.java
+++ src/main/java/org/moera/search/util/UriUtil.java
@@ -5,8 +5,36 @@ import java.net.UnknownHostException;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class UriUtil {
+
+    public static UriComponentsBuilder createBuilderFromRequest(HttpServletRequest request) {
+        UriComponentsBuilder builder = UriComponentsBuilder
+            .fromHttpUrl(request.getRequestURL().toString())
+            .query(request.getQueryString());
+        String forwardedHost = request.getHeader("X-Forwarded-Host");
+        if (!ObjectUtils.isEmpty(forwardedHost)) {
+            builder.host(forwardedHost);
+            String forwardedPort = request.getHeader("X-Forwarded-Port");
+            builder.port(forwardedPort);
+            String forwardedScheme = request.getHeader("X-Forwarded-Proto");
+            if (!ObjectUtils.isEmpty(forwardedScheme)) {
+                builder.scheme(forwardedScheme);
+            }
+        }
+        UriComponents components = builder.build();
+        if (components.getScheme() != null
+            && (
+            components.getScheme().equalsIgnoreCase("https") && components.getPort() == 443
+                || components.getScheme().equalsIgnoreCase("http") && components.getPort() == 80
+        )
+        ) {
+            builder.port(null);
+        }
+        return builder;
+    }
 
     public static InetAddress remoteAddress(HttpServletRequest request) throws UnknownHostException {
         String forwardedAddress = request.getHeader("X-Forwarded-For");
