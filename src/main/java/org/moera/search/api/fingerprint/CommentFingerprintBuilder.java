73085a035d6085684cd0e9cad807a57fc6c27e35
Shmuel Melamud <smelamud@redhat.com>
2026-03-12 03:09:02 +0200

    Correctly validate comment signature version 1.

--- src/main/java/org/moera/search/api/fingerprint/CommentFingerprintBuilder.java
+++ src/main/java/org/moera/search/api/fingerprint/CommentFingerprintBuilder.java
@@ -3,6 +3,7 @@ package org.moera.search.api.fingerprint;
 import java.util.function.Function;
 
 import org.moera.lib.crypto.CryptoUtil;
+import org.moera.lib.crypto.FingerprintException;
 import org.moera.lib.node.Fingerprints;
 import org.moera.lib.node.types.CommentInfo;
 import org.moera.lib.node.types.CommentRevisionInfo;
@@ -10,7 +11,7 @@ import org.moera.search.util.Util;
 
 public class CommentFingerprintBuilder {
 
-    public static final short LATEST_VERSION = 0;
+    public static final short LATEST_VERSION = 1;
 
     public static byte[] build(
         short version,
@@ -19,20 +20,39 @@ public class CommentFingerprintBuilder {
         byte[] postingDigest,
         byte[] repliedToDigest
     ) {
-        return Fingerprints.comment(
-            commentInfo.getOwnerName(),
-            postingDigest,
-            nullDigest(repliedToDigest),
-            commentInfo.getBodySrcHash(),
-            commentInfo.getBodySrcFormat().getValue(),
-            commentInfo.getBody().getEncoded(),
-            commentInfo.getBodyFormat().getValue(),
-            Util.toTimestamp(commentInfo.getRevisionCreatedAt()),
-            (byte) 0,
-            CryptoUtil.digest(
-                AttachmentFingerprintBuilder.build(null, commentInfo.getMedia(), mediaDigest)
-            )
-        );
+        return switch (version) {
+            case 1 ->
+                Fingerprints.comment1(
+                    commentInfo.getOwnerName(),
+                    postingDigest,
+                    repliedToDigest,
+                    commentInfo.getBodySrcHash(),
+                    commentInfo.getBodySrcFormat().getValue(),
+                    commentInfo.getBody().getEncoded(),
+                    commentInfo.getBodyFormat().getValue(),
+                    Util.toTimestamp(commentInfo.getRevisionCreatedAt()),
+                    (byte) 0,
+                    CryptoUtil.digest(
+                        AttachmentFingerprintBuilder.build(null, commentInfo.getMedia(), mediaDigest)
+                    )
+                );
+            case 0 ->
+                Fingerprints.comment0(
+                    commentInfo.getOwnerName(),
+                    postingDigest,
+                    nullDigest(repliedToDigest),
+                    commentInfo.getBodySrcHash(),
+                    commentInfo.getBodySrcFormat().getValue(),
+                    commentInfo.getBody().getEncoded(),
+                    commentInfo.getBodyFormat().getValue(),
+                    Util.toTimestamp(commentInfo.getRevisionCreatedAt()),
+                    (byte) 0,
+                    CryptoUtil.digest(
+                        AttachmentFingerprintBuilder.build(null, commentInfo.getMedia(), mediaDigest)
+                    )
+                );
+            default -> throw new FingerprintException("Unknown fingerprint version: " + version);
+        };
     }
 
     public static byte[] build(
@@ -43,20 +63,39 @@ public class CommentFingerprintBuilder {
         byte[] postingDigest,
         byte[] repliedToDigest
     ) {
-        return Fingerprints.comment(
-            commentInfo.getOwnerName(),
-            postingDigest,
-            nullDigest(repliedToDigest),
-            commentRevisionInfo.getBodySrcHash(),
-            commentRevisionInfo.getBodySrcFormat().getValue(),
-            commentRevisionInfo.getBody().getEncoded(),
-            commentRevisionInfo.getBodyFormat().getValue(),
-            Util.toTimestamp(commentRevisionInfo.getCreatedAt()),
-            (byte) 0,
-            CryptoUtil.digest(
-                AttachmentFingerprintBuilder.build(null, commentInfo.getMedia(), mediaDigest)
-            )
-        );
+        return switch (version) {
+            case 1 ->
+                Fingerprints.comment1(
+                    commentInfo.getOwnerName(),
+                    postingDigest,
+                    repliedToDigest,
+                    commentRevisionInfo.getBodySrcHash(),
+                    commentRevisionInfo.getBodySrcFormat().getValue(),
+                    commentRevisionInfo.getBody().getEncoded(),
+                    commentRevisionInfo.getBodyFormat().getValue(),
+                    Util.toTimestamp(commentRevisionInfo.getCreatedAt()),
+                    (byte) 0,
+                    CryptoUtil.digest(
+                        AttachmentFingerprintBuilder.build(null, commentInfo.getMedia(), mediaDigest)
+                    )
+                );
+            case 0 ->
+                Fingerprints.comment0(
+                    commentInfo.getOwnerName(),
+                    postingDigest,
+                    nullDigest(repliedToDigest),
+                    commentRevisionInfo.getBodySrcHash(),
+                    commentRevisionInfo.getBodySrcFormat().getValue(),
+                    commentRevisionInfo.getBody().getEncoded(),
+                    commentRevisionInfo.getBodyFormat().getValue(),
+                    Util.toTimestamp(commentRevisionInfo.getCreatedAt()),
+                    (byte) 0,
+                    CryptoUtil.digest(
+                        AttachmentFingerprintBuilder.build(null, commentInfo.getMedia(), mediaDigest)
+                    )
+                );
+            default -> throw new FingerprintException("Unknown fingerprint version: " + version);
+        };
     }
 
     private static byte[] nullDigest(byte[] digest) {
